d65f7e3b0bbcdb9b662339bbfb56b140691e0f53
oldcity255 <77759953+oldcity255@users.noreply.github.com>
2021-05-17 02:32:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,58 +1,28 @@
-/**
- *
- * AWS VPC Peering Connection Module
- * =================================
- *
- * Terraform module, which creates a peering connection between two VPCs and adds routes to the local VPC.
- *
- * Usage
- * -----
- *
- * ### Single Region Peering
- * **Notice**: You need to declare both providers even with single region peering.
- *
- * ```hc1
- * module "vpc_single_region_peering" {
- *   source = "dcos-terraform/vpc-peering/aws"
- *
- *   providers = {
- *     aws.local = "aws"
- *     aws.remote = "aws"
- *   }
- *
- *   remote_vpc_id       = "vpc-bbbbbbbb"
- *   remote_subnet_range = "10.0.0.0/16"
- *   local_subnet_range  = "10.1.0.0/16"
- *   local_vpc_id        = "vpc-aaaaaaaa"
- *
- *   tags = {
- *     Environment = "prod"
- *   }
- * }
- * ```
- *
- * ### Cross Region Peering
- *
- * ```hc1
- * module "vpc_cross_region_peering" {
- *   source = "dcos-terraform/vpc-peering/aws"
- *
- *   providers = {
- *     aws.local  = "aws.src"
- *     aws.remote = "aws.dst"
- *   }
- *
- *   remote_vpc_id       = "vpc-bbbbbbbb"
- *   remote_subnet_range = "10.0.0.0/16"
- *   local_subnet_range  = "10.1.0.0/16"
- *   local_vpc_id        = "vpc-aaaaaaaa"
- *
- *   tags = {
- *     Environment = "prod"
- *   }
- * }
- *```
- */
+
+
+  # AWS VPC Peering Connection Module
+#  *=================================
+
+#  Terraform module, which creates a peering connection between two VPCs and adds routes to the local VPC.
+ 
+
+module "vpc_cross_region_peering" {
+  source = "github.com/oldcity255/terraform-aws-vpc-peering"
+
+  providers = {
+    aws.local  = aws.src
+    aws.remote = aws.dst
+  }
+
+  remote_vpc_id       = aws_vpc.remote.id
+  remote_subnet_range = var.remote_subnet_range
+  local_subnet_range  = var.local_subnet_range
+  local_vpc_id        = aws_vpc.local.id
+
+  tags = {
+    Name = "${var.prefix}-vpc_peering"
+  }
+}
 
 # Providers are required because of cross-region
 provider "aws" {
@@ -64,53 +34,53 @@ provider "aws" {
 }
 
 data "aws_region" "remote" {
-  provider = "aws.remote"
+  provider = aws.remote
 }
 
 data "aws_vpc" "local" {
-  provider = "aws.local"
-  id       = "${var.local_vpc_id}"
+  provider = aws.local
+  id       = var.local_vpc_id
 }
 
 data "aws_vpc" "remote" {
-  provider = "aws.remote"
-  id       = "${var.remote_vpc_id}"
+  provider = aws.remote
+  id       = var.remote_vpc_id
 }
 
 # Create a route
 resource "aws_route" "local_rt" {
-  provider                  = "aws.local"
-  route_table_id            = "${coalesce(var.local_main_route_table_id, data.aws_vpc.local.main_route_table_id)}"
-  destination_cidr_block    = "${var.remote_subnet_range}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.peering.id}"
+  provider                  = aws.local
+  route_table_id            = coalesce(var.local_main_route_table_id, data.aws_vpc.local.main_route_table_id)
+  destination_cidr_block    = var.remote_subnet_range
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
 ## Create a route
 resource "aws_route" "remote_rt" {
-  provider                  = "aws.remote"
-  route_table_id            = "${coalesce(var.remote_main_route_table_id, data.aws_vpc.remote.main_route_table_id)}"
-  destination_cidr_block    = "${var.local_subnet_range}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.peering.id}"
+  provider                  = aws.remote
+  route_table_id            = coalesce(var.remote_main_route_table_id, data.aws_vpc.remote.main_route_table_id)
+  destination_cidr_block    = var.local_subnet_range
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
 resource "aws_vpc_peering_connection" "peering" {
-  provider    = "aws.local"
-  vpc_id      = "${var.local_vpc_id}"
-  peer_vpc_id = "${var.remote_vpc_id}"
-  peer_region = "${data.aws_region.remote.name}"
+  provider    = aws.local
+  vpc_id      = var.local_vpc_id
+  peer_vpc_id = var.remote_vpc_id
+  peer_region = data.aws_region.remote.name
 
-  tags = "${merge(var.tags, map("Name", "Initiator: VPC Peering between default and remote",
+  tags = merge(var.tags, map("Name", "Initiator: VPC Peering between default and remote",
                                 "accepter_vpc", var.remote_vpc_id,
-                                "initiator_vpc", var.local_vpc_id))}"
+                                "initiator_vpc", var.local_vpc_id))
 }
 
 # Accepter's side of the connection.
 resource "aws_vpc_peering_connection_accepter" "remote" {
-  provider                  = "aws.remote"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.peering.id}"
+  provider                  = aws.remote
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   auto_accept               = true
 
-  tags = "${merge(var.tags, map("Name", "Accepter",
+  tags = merge(var.tags, map("Name", "Accepter",
                                 "accepter_vpc", var.remote_vpc_id,
-                                "initiator_vpc", var.local_vpc_id))}"
+                                "initiator_vpc", var.local_vpc_id))
 }
